21ad81f8f6cadb0e887e4dcd10533dcfac5d99cd
Bruno Martins <bgcngm@gmail.com>
2024-03-21 16:31:24 +0000

    sm6150-common: Remove deprecated trust HAL
    Change-Id: Ic5ac79b4ca60b87a97622877b97f806c07cab907

--- common.mk
+++ common.mk
@@ -437,10 +437,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.sm6150
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0.vendor \
