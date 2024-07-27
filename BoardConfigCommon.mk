2512c90e128a952b98fc5e3e47c1808556f9e36a
Andrzej Szafran <rtd1250@gmail.com>
2024-07-27 13:19:57 +0200

    sm6150-common: Include hardware/samsung FCM
    Change-Id: I9184a9393708197301c7d97c18f9a2710e25aa32

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -138,6 +138,7 @@ BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := false
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
